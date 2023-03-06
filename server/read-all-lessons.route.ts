cb1baf3d71fbef17ea05b5c11bf0252b84fdc774
marceloungaretti <marcelo.ungaretti@gmail.com>
2023-03-06 18:02:03 -0300

    fim da aula 28

--- server/read-all-lessons.route.ts
+++ server/read-all-lessons.route.ts
@@ -1,9 +1,14 @@
-
-import {db} from "./database";
-
+import { db } from "./database";
+import { sessionStore } from "./session-store";
 
 export function readAllLessons(req, res) {
+  const sessionId = req.cookies['SESSIONID'];
+  const isSessionValid = sessionStore.isSessionValid(sessionId);
 
-    res.status(200).json(db.readAllLessons());
+  if (!isSessionValid) {
+    res.sendStatus(403);
+  } else {
+    res.status(200).json({ lessons: db.readAllLessons() });
+  }
 
 }
