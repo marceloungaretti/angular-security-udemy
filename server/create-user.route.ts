e3522e3f8f0a304419ad496d0a1373afceb97a69
marceloungaretti <marcelo.ungaretti@gmail.com>
2023-03-10 20:34:27 -0300

    fim da aula 39

--- server/create-user.route.ts
+++ server/create-user.route.ts
@@ -3,6 +3,7 @@ import { db } from "./database";
 import * as argon2 from 'argon2';
 import { validatePassword } from "./password-validation";
 import moment = require("moment");
+import { createSessionToken } from "./security.utils";
 
 export function createUser(req: Request, res: Response) {
   const credentials = req.body;
@@ -20,8 +21,8 @@ export function createUser(req: Request, res: Response) {
 async function createUserAndSession(res: Response, credentials) {
   const passwordDigest = await argon2.hash(credentials.password);
   const user = db.createUser(credentials.email, passwordDigest);
-  // TODO replace with JWT
-  const sessionToken = 1;
+
+  const sessionToken = await createSessionToken(user.id.toString());
 
   res.cookie("SESSIONID", sessionToken, { httpOnly: true, secure: true });
   res.status(200).json({ id: user.id, email: user.email });
--- server/security.utils.ts
+++ server/security.utils.ts
@@ -1,22 +1,21 @@
-
-
-
 import moment = require("moment");
 const util = require('util');
 const crypto = require('crypto');
 import * as jwt from 'jsonwebtoken';
 import * as fs from "fs";
 
-
-
 export const randomBytes = util.promisify(crypto.randomBytes);
 
-
+export const signJwt = util.promisify(jwt.sign);
 
 const RSA_PRIVATE_KEY = fs.readFileSync('./demos/private.key');
-
 const RSA_PUBLIC_KEY = fs.readFileSync('./demos/public.key');
-
 const SESSION_DURATION = 240;
 
-
+export async function createSessionToken(userId: string) {
+  return signJwt({}, RSA_PRIVATE_KEY, {
+    algorithm: 'RS256',
+    expiresIn: SESSION_DURATION,
+    subject: userId
+  });
+}
