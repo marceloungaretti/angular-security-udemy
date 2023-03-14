52a55b91ae44b8d5abdb283b4f69a027d0aa73d9
marceloungaretti <marcelo.ungaretti@gmail.com>
2023-03-14 01:45:56 -0300

    fim da aula 51

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,60 +1,57 @@
-
-import {filter} from 'rxjs/operators';
-import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable, BehaviorSubject} from "rxjs";
-import {User} from "../model/user";
+import { filter } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable, BehaviorSubject } from "rxjs";
+import { User } from "../model/user";
 import * as auth0 from 'auth0-js';
-import {Router} from "@angular/router";
+import { Router } from "@angular/router";
+import { environment } from '../../environments/environment';
 
 export const ANONYMOUS_USER: User = {
-    id: undefined,
-    email: ''
+  id: undefined,
+  email: ''
 };
 
 const AUTH_CONFIG = {
-    clientID: 'hHhF4PWGY7vxLQH2HatJaUOertB1dDrU',
-    domain: "angularsecuritycourse.auth0.com"
+  clientID: environment.clientID,
+  domain: environment.domain
 };
 
 
 @Injectable()
 export class AuthService {
 
-    auth0 = new auth0.WebAuth({
-        clientID: AUTH_CONFIG.clientID,
-        domain: AUTH_CONFIG.domain,
-        responseType: 'token id_token',
-        redirectUri: 'https://localhost:4200/lessons'
-    });
+  auth0 = new auth0.WebAuth({
+    clientID: AUTH_CONFIG.clientID,
+    domain: AUTH_CONFIG.domain,
+    responseType: 'token id_token',
+    redirectUri: 'https://localhost:4200/lessons'
+  });
 
-    private userSubject = new BehaviorSubject<User>(undefined);
+  private userSubject = new BehaviorSubject<User>(undefined);
+  user$: Observable<User> = this.userSubject.asObservable().pipe(filter(user => !!user));
 
-    user$: Observable<User> = this.userSubject.asObservable().pipe(filter(user => !!user));
+  constructor(private http: HttpClient, private router: Router) { }
 
-    constructor(private http: HttpClient, private router: Router) {
+  login() {
+    this.auth0.authorize({});
+  }
 
-    }
+  signUp() {
 
-    login() {
+  }
 
-    }
+  logout() {
 
-    signUp() {
+  }
 
-    }
+  public isLoggedIn() {
+    return false;
+  }
 
-    logout() {
-
-    }
-
-    public isLoggedIn() {
-        return false;
-    }
-
-    isLoggedOut() {
-        return !this.isLoggedIn();
-    }
+  isLoggedOut() {
+    return !this.isLoggedIn();
+  }
 
 }
 
